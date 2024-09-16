7b931bbbf6f03e93d006ab2cc006181ec113edea
Sebastián Guzmán <sebastian.guzman@outlook.com>
2024-09-16 01:45:52 -0300

    Fix

--- src/Resume/Program.cs
+++ src/Resume/Program.cs
@@ -2,11 +2,11 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Resume;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+WebAssemblyHost app = builder.Build();
 
-await builder.Build().RunAsync();
+await app.RunAsync();
